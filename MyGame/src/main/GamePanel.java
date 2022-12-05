3d53d4666943e982eb251e40e3e3e1102defd27e
yizhou82 <yizhou11182@gmail.com>
2022-12-04 17:47:13 -0800

    Started GamePanel and main, along with implementing game loop and taking keyboard inputs

---
+++ MyGame/src/main/GamePanel.java
@@ -0,0 +1,142 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    //Screen settings, change as you like
+    final int originalTileSize = 16; // 16 x 16 tile for characters
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; //Will look like 48 x 48 on screen
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; //768 pixels
+    final int screenHeight = tileSize * maxScreenRow; //576 pixels
+
+    // FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+
+    // Set player's default position
+    //In java (0,0) starts at the top left corder of the screen, so
+    // x values increase as they go right
+    // y values increase as they go down
+    int playerX = 100;
+    int playerY = 100;
+
+    int playerSpeed = 4;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+//    Sleep GameLoop
+//    @Override
+//    public void run() {
+//
+//        double drawInterval = 1000000000/FPS; // 0.01666 sec
+//        double nextDrawTime = System.nanoTime() + drawInterval;
+//
+//        while(gameThread != null) {
+//
+//            // 1 Update: update info such as character position
+//            update();
+//            // 2 Draw: draw updated info on screen
+//            repaint();
+//
+//            try {
+//                double remainingTime = nextDrawTime - System.nanoTime();
+//                remainingTime /= 1000000;
+//
+//                if(remainingTime < 0) {
+//                    remainingTime = 0;
+//                }
+//
+//                Thread.sleep((long) remainingTime);
+//
+//                nextDrawTime += drawInterval;
+//
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//
+//    }
+
+
+    //Delta GameLoop
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if(timer >= 1000000000) {
+                System.out.println("FPS:" + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update() {
+
+        //Add else to each statement to stop diagonal motion
+        //It may interfere with animations
+        if(keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        }
+        if(keyH.downPressed == true) {
+            playerY += playerSpeed;
+        }
+        if(keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        }
+        if(keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
+    }
+}
---
+++ MyGame/src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import javax.swing.JFrame;
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.getDefaultCloseOperation();
+        window.setResizable(false);
+        window.setTitle("2D Horror");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
